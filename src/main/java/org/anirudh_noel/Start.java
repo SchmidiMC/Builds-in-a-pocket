b3a472f01d47c66a53bd6d3c4110cee8b372b465
DorSchmidi <noelschmieder01@gmail.com>
2020-10-28 21:59:06 +0100

    fixed initial commit

--- src/main/java/org/anirudh_noel/Start.java
+++ src/main/java/org/anirudh_noel/Start.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import de.schmidimc.mc.common.annotation.ApiVersion;
 import de.schmidimc.mc.common.annotation.plugin.Plugin;
 
-@Plugin(name = "BuildsInAPocket", apiVersion = ApiVersion.VERSION_1_16, authors = {"Minecraftian14", "Schmidi"}, main = "de.anirudh_noel.Start", version = "1.6.13", description = "A fun plugin")
+@Plugin(name = "BuildsInAPocket", apiVersion = ApiVersion.VERSION_1_16, authors = {"Minecraftian14", "Schmidi"}, main = "org.anirudh_noel.Start", version = "1.6.13", description = "A fun plugin")
 public class Start extends JavaPlugin {
 
 	@Override
